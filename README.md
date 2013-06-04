1b0affb75f5e191eb1a8d738cc0d44115f0d9b54
Tjaart van der Walt <github@tjaart.co.za>
2013-06-04 15:19:02 -0500

    updated quite a bit... need to add finishing touches

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-photo_renamer
-=============
+# Photo Renamer #
+## What the script does ##
+This script is fairly specific. It renames all .jpg files in a given directory to the form
+IMG_*yyyymmdd*_*hhmmss*.jpg
 
-rename the .jpg files in a directory to have the naming pattern IMG_yyyymmdd_hhmmss.jpg
+## Rationale ##
+The naming pattern described above is the naming pattern used by the stock Android camera.
+This naming pattern allows me to seamlessly merge me photos taken from different devices into a directory and maintain chronology.
+Unfortunately my camera does not support this naming pattern, and thus the need for this script.
+
+## mtime ##
+After some experimenting I found that if I copy photos from my camera's memory card to my PC the "Modify Time" field gets preserved. Therefore I use this field as the basis for renaming the files. Obviously if you modify the file before renaming the mtime field will get updated and your file name will be wrong, so be warned!
+
+## usage ##
+
+
+## GNU GPL ##
+This program is licensed under [GNU GPL](http://www.gnu.org/licenses/gpl-3.0.txt)
--- photo_renamer.rb
+++ photo_renamer.rb
@@ -1,18 +1,87 @@
-mdate = File.mtime("testfile")
+#!/usr/bin/env ruby
 
-puts mdate
+# photo_renamer renames all .jpg files in a given directory to the form IMG_*yyyymmdd*_*hhmmss*.jpg
+# Copyright (C) 2013  Tjaart van der Walt
 
-folder_path = "/home/tjaart/dev/git/photo_renamer/photos"
-Dir.glob(folder_path + "/*").sort.each do |f|
-    filename = File.basename(f, File.extname(f))
-    File.rename(f, filename.capitalize + File.extname(f))
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'optparse'
+require 'awesome_print'
+
+def new_name(mdate)
+    ret_string = "IMG_"
+    ret_string += "#{mdate.year}#{("%02d" % mdate.month).to_s}#{("%02d" % mdate.day).to_s}"
+    ret_string += "_#{("%02d" % mdate.hour).to_s}#{("%02d" % mdate.min).to_s}#{("%02d" % mdate.sec).to_s}"
+    ret_string += ".jpg"
+    return ret_string
+end
+    
+def name_map(dir_path)
+    name_map = {}
+    Dir.glob("#{dir_path}/*.[jJ][pP][gG]").sort.each do |f|
+        mdate = File.mtime(f)
+        name_map[f] = "#{dir_path}/#{new_name(mdate)}"
+    end
+    return name_map
 end
 
-puts mdate.year
-puts "%02d" % mdate.month
-puts "%02d" % mdate.day
+def print_replace_names(dir_path, map)
+    puts "We will be renaming the following image files"
+    ap map
+    puts "Are you sure you want to continue renaming these files (Y/N)"
+    if STDIN.gets.chomp.downcase == "y"
+        map.each() do |key, value|
+            File.rename("#{key}", "#{value}")          
+        end
+    end
+end
 
-puts "%02d" % mdate.hour
-puts "%02d" % mdate.min
-puts "%02d" % mdate.sec
+def parse_options(print_opts)
+    options = {}
+ 
+    optparse = OptionParser.new do|opts|
+        opts.banner = "Usage: photo_renamer.rb [options] dir1 [dir2 ...]"
+ 
+        options[:batch] = false
+        opts.on( '-b', '--batch', 'Batch mode. Will not ask confirmation before renaming.' ) do
+            options[:batch] = true
+        end 
+   
+        opts.on( '-h', '--help', 'Display this screen' ) do
+            puts opts
+            exit
+        end
+    end
+
+    if print_opts
+        puts print_opts
+        puts optparse
+    end
+    optparse.parse!
+    return options
+end
+
+def main()
+    ARGV.length == 0 ? print_opts = true : print_opts = false
+    opts = parse_options(print_opts)
+
+    ARGV.each do |arg|
+        map = name_map(arg)
+        print_replace_names(arg, map)
+    end
+
+end
+
+main()
 
